1401e09b721d4f5c6c9ebc3b764eacc15f178508
josehkgumiero <jose_hkg@hotmail.com>
2026-01-04 15:45:50 -0300

    AtualizaÃ§Ã£o automÃ¡tica - 2026-01-04 15:45:48

--- scripting-git-push-all-repositories.ps1
+++ scripting-git-push-all-repositories.ps1
@@ -45,8 +45,6 @@ foreach ($repo in $repositorios) {
         # Push para a branch atual
         git push -u origin main
         Write-Host "Commit e push realizados com sucesso!" -ForegroundColor Green
-        cd ..
-        cd '.\powershell\'
     }
     catch {
         Write-Host "Erro ao processar $repoPath" -ForegroundColor Red
